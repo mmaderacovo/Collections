c70980615774026ba3edd445c2bef876d1ba6f01
Angelica <mmaderacovo@gmail.com>
2023-04-17 15:53:31 -0500

    Iniciando Collections

---
+++ src/main/java/org/amadera/ejemplos/set/EjemploHashSetAgregar.java
@@ -0,0 +1,18 @@
+package org.amadera.ejemplos.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EjemploHashSetAgregar {
+    public static void main(String[] args) {
+
+        Set<String> hs = new HashSet<>();
+        hs.add("uno");
+        hs.add("dos");
+        hs.add("tres");
+        hs.add("cuatro");
+        hs.add("cinco");
+
+        System.out.println(hs);
+    }
+}
