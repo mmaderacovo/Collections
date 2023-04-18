073380715eda6e0283fcd4d578102e36f9ad7648
Angelica <mmaderacovo@gmail.com>
2023-04-18 15:19:23 -0500

    HashSetUnicidad

--- src/main/java/org/amadera/ejemplos/modelo/Alumno.java
+++ src/main/java/org/amadera/ejemplos/modelo/Alumno.java
@@ -1,5 +1,7 @@
 package org.amadera.ejemplos.modelo;
 
+import java.util.Objects;
+
 public class Alumno implements Comparable<Alumno> {
     private String nombre;
     private Integer nota;
@@ -48,5 +50,19 @@ public class Alumno implements Comparable<Alumno> {
             return 0;
         }
         return this.nota.compareTo(a.nota);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(nombre, alumno.nombre) &&
+                Objects.equals(nota, alumno.nota);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, nota);
     }
 }
--- src/main/java/org/amadera/ejemplos/set/EjemploHashSetUnicidad.java
+++ src/main/java/org/amadera/ejemplos/set/EjemploHashSetUnicidad.java
@@ -2,20 +2,20 @@ package org.amadera.ejemplos.set;
 
 import org.amadera.ejemplos.modelo.Alumno;
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
-public class EjemploTreeSetComparable {
+public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
 
-        Set<Alumno> sa = new TreeSet<>((a,b)-> a.getNombre().compareTo(b.getNombre()));
+        Set<Alumno> sa = new HashSet<>();
         sa.add(new Alumno("Diana", 5));
         sa.add(new Alumno("Rey", 6));
         sa.add(new Alumno("Danna", 7));
         sa.add(new Alumno("Ana", 3));
         sa.add(new Alumno("Camilo", 10));
         sa.add(new Alumno("Paula", 2));
-        sa.add(new Alumno("Pamela", 2));
+        sa.add(new Alumno("Paula", 3));
 
         System.out.println(sa);
     }
