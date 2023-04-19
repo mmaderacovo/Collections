5efde3e85901e6061a92b3f6e6985c5ec4cdc6a8
Angelica <mmaderacovo@gmail.com>
2023-04-19 15:59:15 -0500

    Principales metodos linkedList

---
+++ src/main/java/org/amadera/ejemplos/set/EjemploLinkedList.java
@@ -0,0 +1,50 @@
+package org.amadera.ejemplos.set;
+
+import org.amadera.ejemplos.modelo.Alumno;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EjemploLinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<Alumno> lili = new LinkedList<>();
+        System.out.println(lili+ " , size "+ lili.size());
+        System.out.println("Esta vacia la lista "+ lili.isEmpty());
+        lili.add(new Alumno("Diana", 5));
+        lili.add(new Alumno("Rey", 6));
+        lili.add(new Alumno("Danna", 7));
+        lili.add(new Alumno("Ana", 3));
+        lili.add (new Alumno("Camilo", 10));
+        lili.add(new Alumno("Paula", 2));
+
+        System.out.println(lili+ " , size "+ lili.size());
+
+        lili.addFirst(new Alumno("Carolina",7));
+        lili.addLast(new Alumno("Juan",9));
+        System.out.println(lili+ " , size "+ lili.size());
+
+        System.out.println("Primero: "+ lili.getFirst());
+        System.out.println("Ultimo: " + lili.getLast());
+        System.out.println("Indice = " + lili.get(3));
+
+        Alumno carolina = lili.removeFirst();
+        lili.removeLast();
+        System.out.println(lili+ " , size "+ lili.size());
+
+        Alumno a = new Alumno("Catalina",7);
+        lili.addLast(a);
+        System.out.println("Indice de Catalina: "+ lili.indexOf(a));
+
+        lili.remove(3);
+        System.out.println(lili+ " , size "+ lili.size());
+
+        lili.set(3, new Alumno("Pedro", 8));
+        System.out.println(lili+ " , size "+ lili.size());
+
+
+
+
+    }
+}
